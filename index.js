3f82b00a56122c9e01ea4a1602f1bb0ed657d70b
mia-sv <130096279+mia-sv@users.noreply.github.com>
2023-09-05 23:24:41 +0100

    improvements to post insertion

--- index.js
+++ index.js
@@ -1,47 +1,88 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+let posts = [];
+const inputEl = document.getElementById('input-el');
+const ulEl = document.getElementById('ul-el');
+const saveBtn = document.getElementById('save-btn');
+const saveTabBtn = document.getElementById('save-tab-btn');
+
+// Get posts from local storage, iff they exist
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('posts'));
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+  posts = leadsFromLocalStorage;
+  render();
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
+// Function to render all posts
+function render() {
+  let postsHTML = '';
 
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+  for (let i = 0; i < posts.length; i++) {
+    const text = posts[i].text;
+
+    if ('url' in posts[i] && 'text' in posts[i]) {
+      postsHTML += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                <a target='_blank' href='${posts[i].url}'>
+                    ${posts[i].text}
                 </a>
             </li>
-        `
+        `;
+    } else if ('text' in posts[i]) {
+      postsHTML += `<li>${posts[i].text}</li>`;
+    } else {
+      postsHTML += `
+                <li>
+                    <a target='_blank' href='${posts[i].url}'>
+                        ${posts[i].url} 
+                    </a>
+                </li>
+            `;
     }
-    ulEl.innerHTML = listItems
+  }
+
+  
+
+  ulEl.innerHTML = postsHTML;
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
+// Event listeners ---------------------------
+saveTabBtn.addEventListener('click', () => {
+  new Promise((resolve, reject) => {
+    resolve(browser.tabs.query({ active: true, currentWindow: true }));
+  })
+    .catch(() => {
+      console.log('browser.tabs unavailable, fetching url with chrome.tabs...');
+      chrome.tabs.query({ active: true, currentWindow: true });
+    })
+    .then((tabs) => tabs[0].url)
+    .catch(() => {
+      console.log(
+        'chrome.tabs unavailable, fetching url with window.location.href...'
+      );
+      return window.location.href;
+    })
+    .then((url) => {
+      const text = inputEl.value.trim();
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-})
+      if (text.length != 0) {
+        posts.push({ text: inputEl.value, url: url });
+      } else {
+        posts.push({ url: url });
+      }
+
+      inputEl.value = '';
+      localStorage.setItem('posts', JSON.stringify(posts));
+      render();
+    });
+});
+
+saveBtn.addEventListener('click', () => {
+  const text = inputEl.value.trim();
+
+  if (text.length != 0) {
+    posts.push({ text: text });
+    inputEl.value = '';
+    localStorage.setItem('posts', JSON.stringify(posts));
+    render();
+  }
+});
